5c6262bbc8bba780da9d7bfdaa62db2cb25901f1
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-08-19 19:34:52 -0600

    Gauss-Jordan

--- Parcial1.py
+++ Parcial1.py
@@ -1,12 +1,9 @@
 import numpy as np
+import os
 #Esto es una calculadora de sistemas de ecuaciones usando el metodo de Gauss Seidel
-
 flag = True
 
-while True:
-    print("Este programa resuleve sistemas de ecuaciones cuadrados usando el método de Gauss Seidel")
-
-    #Se crea el arreglo de nxn primero con ceros
+def Gauss_Seidel():
     size = int(input("Ingresa el tamaño del sistema de ecuaciones: "))
     matrizA = np.zeros((size, size), dtype=float)
     matrizB = np.zeros(size, dtype=float)
@@ -25,7 +22,7 @@ while True:
 
     Error_Deseado = float(input("Ingresa el error deseado: "))
 
-    
+
     #Verificamos que la diagonal sea diferente a cero
     diagonal = np.diagonal(matrizA)
 
@@ -36,7 +33,7 @@ while True:
         if (diagonal[i] == 0.0):
             raise ValueError(f"El elemento de la diagonal {i} es 0. Considera reorganiar las filas")
 
-"""
+    """
 
     while np.any(np.abs(Errors) > Error_Deseado):
 
@@ -56,10 +53,52 @@ while True:
         for i in range(size):
             Errors[i] = np.abs((valuesX[i] - PastValues[i]) / valuesX[i]) * 100
 
+
+    for i in range(size):
+        print(f"El valor de X{i+1} es: {valuesX[i]}")
+
+def Gaussiana():
+    size = int(input("Ingresa el tamaño del sistema de ecuaciones"))
+    matrizA = np.zeros((size, size + 1), dtype=float)
     
-    print(valuesX)
-    print(Errors)
-    print(iteracion)
+    for i in range(size):
+        for j in range(size + 1):
+            matrizA[i , j] = float(input(f"Ingresa el valor de la posicion ({i+1}, {j+1}): "))
+
+    pivote = matrizA[0,0]
+
+    for i in range(size):
+        matrizA[i] = matrizA[i] / matrizA[i,i]
+
+        for j in range(size):
+            if( i != j):
+                matrizA[j] = matrizA[j] - matrizA[i] * matrizA[j,i]
+
+    print(matrizA)
+
+
+
+def menu():
+    print("1. Calculadora mediante Gauss Seidel")
+    print("2. Calculadora mediante montante")
+
+
+while flag:
+    os.system('cls')
+
+    menu()
+    opcion = int(input("Ingresa la opción deseada: "))
+    if opcion == 1:
+        Gauss_Seidel()
+    if opcion == 2:
+        Gaussiana()
+
+
+    resp = input("¿Quieres ingresar otro sistema de ecuaciones? (S/N): ")
+    if(resp == "N" or resp == "n"):
+        flag = False
+
+    
 
 
         
